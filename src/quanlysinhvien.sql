07cbb6a2ee51a886450a88291a83397e4d97b6ca
tranngocdai <85327489+tranngocdai@users.noreply.github.com>
2021-08-17 15:43:24 +0700

    quanlysinhvien

--- src/quanlysinhvien.sql
+++ src/quanlysinhvien.sql
@@ -1,33 +1,56 @@
 CREATE Schema quanlysinhvien;
 USE quanlysinhvien;
 CREATE table Class(
-    ClassID int not null auto_increment primary key,
-    ClassName varchar(60) not null,
-    StartDate datetime not null,
-    Status Bit
+                      ClassID int not null auto_increment primary key,
+                      ClassName varchar(60) not null,
+                      StartDate datetime not null,
+                      Status Bit
 );
 CREATE table Student(
-    StudentID int not null auto_increment primary key,
-    StudentName varchar(30) not null,
-    Address varchar(50),
-    Phone varchar(20),
-    Status Bit,
-    ClassID int not null,
-    foreign key (ClassID) references Class(ClassID)
+                        StudentID int not null auto_increment primary key,
+                        StudentName varchar(30) not null,
+                        Address varchar(50),
+                        Phone varchar(20),
+                        Status Bit,
+                        ClassID int not null,
+                        foreign key (ClassID) references Class(ClassID)
 );
-CREATE Subject(
-    SubID int not null auto_increment primary key,
-    SubName varchar(30) not null,
-    Credit tinyint not null default 1 (Credit Check >=1),
-    Status Bit default 1
+CREATE table Subject(
+                        SubID int not null auto_increment primary key,
+                        SubName varchar(30) not null,
+                        Credit tinyint not null default 1 CHECK(Credit >=1),
+                        Status Bit default 1
 );
 CREATE table Mark(
-    MarkID int not null auto_increment primary key,
-    SubID int not null ,
-    StudentID int not ,
-    Mark Float Default 0 Check (Mark between 0 and 100),
-    ExampleTimes tinyint default 1,
-    Unique(SubID,StudentID),
-    foreign key (StudentID) references Student (StudentID),
-    foreign key (SubID) references Subject(SubID)
+                     MarkID int not null auto_increment primary key,
+                     SubID int not null ,
+                     StudentID int  ,
+                     Mark Float Default 0 Check (Mark between 0 and 100),
+                     ExampleTimes tinyint default 1,
+                     Unique(SubID,StudentID),
+                     foreign key (StudentID) references Student (StudentID),
+                     foreign key (SubID) references Subject(SubID)
 );
+Insert Into Class(ClassID, ClassName, StartDate, Status) Values
+(1,'A1','2008-12-20',1),(2, 'A2', '2008-12-22', 1),(3, 'B3', current_date, 0);
+
+Insert Into Student(StudentName, Address, Phone, Status, ClassID) VALUES
+('Hung', 'Ha Noi', '0912113113', 1, 1);
+
+Insert Into Student( studentname, address,  status, classid) VALUES
+('Hoa', 'Hai phong', 1, 1);
+
+INSERT INTO Student(StudentName, Address, Phone, Status, ClassID) VALUES
+('Manh', 'HCM', '0123123123', 0, 2);
+
+INSERT INTO Subject(subid, subname, credit, status) VALUES
+(1, 'CF', 5, 1),
+(2, 'C', 6, 1),
+(3, 'HDJ', 5, 1),
+(4, 'RDBMS', 10, 1);
+
+INSERT INTO Mark(subid, studentid, mark, exampletimes) VALUES
+(1, 1, 8, 1),
+(1, 2, 10, 2),
+(2, 1, 12, 1);
+
--- src/test.sql
+++ src/test.sql
@@ -1,96 +1,104 @@
 create schema StudentManager;
-use
-StudentManager;
-create table Address
-(
-    AddressID   int auto_increment primary key,
-    AddressName varchar(100)
+use StudentManager;
+create table Address(
+                        AddressID int auto_increment primary key ,
+                        AddressName varchar(100)
 );
-create table Classes
-(
-    ClassesID   int auto_increment primary key,
-    ClassesName varchar(50),
-    language    varchar(50),
-    description varchar(200)
+create table Classes(
+                        ClassesID int auto_increment primary key ,
+                        ClassesName varchar(50),
+                        language varchar(50),
+                        description varchar(200)
 );
-create table Students
-(
-    StudentID int auto_increment primary key,
-    fullName  varchar(50),
-    AddressID int,
-    phone     int unique,
-    ClassesId int,
-    foreign key (AddressID) references Address (AddressID),
-    foreign key (ClassesID) references Classes (ClassesID)
+create table Students(
+                         StudentID int auto_increment primary key ,
+                         fullName varchar(50),
+                         AddressID int,
+                         phone int unique,
+                         ClassesId int,
+                         foreign key (AddressID) references Address(AddressID),
+                         foreign key (ClassesID) references Classes(ClassesID)
 );
 
-create table Course
-(
-    CourseID    int auto_increment primary key,
-    CourseName  varchar(50),
-    description varchar(200)
+create table Course(
+                       CourseID int auto_increment primary key,
+                       CourseName varchar(50),
+                       description varchar(200)
 );
-create table Point
-(
-    id        int auto_increment primary key,
-    CourseID  int,
-    StudentID int,
-    point     int,
-    foreign key (CourseID) references Course (CourseID),
-    foreign key (Studentid) references Students (StudentID)
+create table Point(
+                      id int auto_increment primary key,
+                      CourseID int,
+                      StudentID int,
+                      point int,
+                      foreign key (CourseID) references Course(CourseID),
+                      foreign key (Studentid) references Students(StudentID)
 );
 
-insert into Address(AddressName)
-values (''Hà Nội''),
-       (''Hồ Chí Minh''),
-       (''Lào''),
-       (''Cambodia''),
-       (''Singapore'');
+insert into Address(AddressName) values
+('Hà Nội'),('Hồ Chí Minh'),('Lào'),('Cambodia'),('Singapore');
 
-insert into Classes(ClassesName, language, description)
-VALUES (''A'', ''English'', ''Lớp tiếng Anh''),
-       ('' B'', ''French'', ''Lớp tiếng Pháp''),
-       (''C'', ''Chinese'', ''Lớp tiếng Tàu''),
-       (''D'', ''Korean'', ''Lớp tiếng Hàn''),
-       (''E'', ''Japanese'', ''Lớp tiếng Nhật'');
+insert into Classes(ClassesName, language, description) VALUES
+('A','English','Lớp tiếng Anh'),
+('B','French','Lớp tiếng Pháp'),
+('C','Chinese','Lớp tiếng Tàu'),
+('D','Korean','Lớp tiếng Hàn'),
+('E','Japanese','Lớp tiếng Nhật');
 
-insert into Students(fullName, AddressID, phone, ClassesId)
-VALUES (''Trần Ngọc Đại'', ''1'', ''0123456789'', ''1''),
-       (''Vũ Văn Bình'', ''2'', ''0123456788'', ''2''),
-       (''Mai Đức Trung'', ''3'', ''0123456787'', ''3''),
-       (''Nguyễn Văn Thản'', ''4'', ''0123456786'', ''4''),
-       (''Hoàng Mạnh Cường'', ''5'', ''0123456785'', ''5''),
-       (''Đỗ Xuân Văn'', ''1'', ''0123456784'', ''1''),
-       (''Nguyễn Huy Nam'', ''2'', ''0123456783'', ''2''),
-       (''Đỗ Hoài Nam'', ''3'', ''0123456782'', ''3''),
-       (''Vũ Thị Kiều Anh'', ''4'', ''0123456781'', ''4''),
-       (''Nguyễn Đồng Chính'', ''5'', ''0123456780'', ''5'');
+insert into Students(fullName, AddressID, phone, ClassesId) VALUES
+('Trần Ngọc Đại','1','0123456789','1'),
+('Vũ Văn Bình','2','0123456788','2'),
+('Mai Đức Trung','3','0123456787','3'),
+('Nguyễn Văn Thản','4','0123456786','4'),
+('Hoàng Mạnh Cường','5','0123456785','5'),
+('Đỗ Xuân Văn','1','0123456784','1'),
+('Nguyễn Huy Nam','2','0123456783','2'),
+('Đỗ Hoài Nam','3','0123456782','3'),
+('Vũ Thị Kiều Anh','4','0123456781','4'),
+('Nguyễn Đồng Chính','5','0123456780','5');
 
-insert into Course(CourseName, description)
-VALUES (''So Easy'', ''Mới''),
-       (''Easy'', ''Bắt đầu''),
-       (''Normal'', ''Đã biết''),
-       (''Difficulty'', ''Thông thạo''),
-       (''Very Difficulty'', ''Master'');
+insert into Course(CourseName, description) VALUES
+('So Easy','Mới'),
+('Easy','Bắt đầu'),
+('Normal','Đã biết'),
+('Difficulty','Thông thạo'),
+('Very Difficulty','Master');
 
-insert into Point(CourseID, StudentID, point)
-values (''1'', ''1'', ''10''),
-       (''2'', ''5'', ''9''),
-       (''3'', ''3'', ''5''),
-       (''4'', ''2'', ''1''),
-       (''5'', ''4'', ''0''),
-       (''5'', ''6'', ''7''),
-       (''4'', ''7'', ''6''),
-       (''3'', ''10'', ''3''),
-       (''2'', ''9'', ''2''),
-       (''1'', ''1'', ''8''),
-       (''1'', ''3'', ''7''),
-       (''2'', ''4'', ''6''),
-       (''3'', ''6'', ''5''),
-       (''4'', ''8'', ''9''),
-       (''5'', ''7'', ''10'');
-#
-Thống kê số lượng HV các lớp
-select ClassesName, count(ClassesName) ''Số lượng học viên các lớp''
+insert into Point(CourseID,StudentID,point) values
+('1','1','10'),
+('2','5','9'),
+('3','3','5'),
+('4','2','1'),
+('5','4','0'),
+('5','6','7'),
+('4','7','6'),
+('3','10','3'),
+('2','9','2'),
+('1','1','8'),
+('1','3','7'),
+('2','4','6'),
+('3','6','5'),
+('4','8','9'),
+('5','7','10');
+# Thống kê số lượng HV các lớp
+select ClassesName, count(ClassesName)
 from classes join students s on classes.ClassesID = s.ClassesId
 group by ClassesName;
+# Thống kê số lượng học viên tại các tỉnh
+select AddressName, count(AddressName)
+from address join Students s on Address.AddressID = s.AddressID
+group by AddressName;
+# Tính điểm trung bình của các khóa học
+select CourseName, avg(point)
+from point join Course C on Point.CourseID = C.CourseID
+group by CourseName;
+# Đưa ra khóa học có điểm trung bình cao nhất
+select CourseName,avg(point) as avg_point
+from point join Course C on C.CourseID = Point.CourseID
+group by CourseName having avg_point >= All(
+    SELECT avg(point) as avg_point from point
+                                            join Course C2 on C2.CourseID = Point.CourseID
+    group by C.CourseName
+)
+;
+
+
